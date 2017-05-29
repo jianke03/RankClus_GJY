68d5ef4ebd477757edc5bfecc83c9005cefa27ab
gaojingyue_i <gaojingyue_i@didichuxing.com>
2017-05-29 12:58:45 +0800

    add parallel

--- code/rankClus_pl.py
+++ code/rankClus_pl.py
@@ -2,26 +2,36 @@ from base import buildNet, initializeCluster, authorityRank, simpleRank
 from base import clusterReassign, EM, checkNull
 import heapq
 import numpy as np
+import multiprocessing as mp
+
 T = 2
 K = 15
 rankT = 10
 alpha = 0.95
 EMT = 5
-author_confer, confer_author, author_author = buildNet('data/refined_info.txt')
+author_confer, confer_author, author_author = buildNet('../data/refined_info.txt')
 cluster = initializeCluster(confer_author.keys(), K)
 rankclus_iter = 0
+manager = mp.Manager()
+
+def rank(i,author_score_cluster,confer_score_cluster,confer_score_in_cluster,clu):
+    print('in ranking cluster ' + str(i))
+    author_score_cluster[i], confer_score_cluster[i], confer_score_in_cluster[i] = authorityRank(author_confer, confer_author, author_author, clu, rankT,alpha)
+
 while rankclus_iter < T:
-    author_score_cluster = {}
-    confer_score_cluster = {}
-    confer_score_in_cluster = {}
+    author_score_cluster = manager.dict()
+    confer_score_cluster = manager.dict()
+    confer_score_in_cluster = manager.dict()
     print('start ranking')
+    pool = mp.Pool(processes=K)
     for i in range(K):
-        print('in ranking cluster ' + str(i))
-        author_score_cluster[i], confer_score_cluster[
-            i], confer_score_in_cluster[i] = authorityRank(
-                author_confer, confer_author, author_author, cluster[i], rankT,
-                alpha)
-    print('start EM')
+        pool.apply_async(rank, (i,author_score_cluster,confer_score_cluster,confer_score_in_cluster,cluster[i]))
+    pool.close()
+    pool.join()
+    author_score_cluster = dict(author_score_cluster)
+    confer_score_cluster = dict(confer_score_cluster)
+    
+    print('start EM')    
     Pro_confer_cluster = EM(confer_author, confer_score_cluster,
                             author_score_cluster, cluster, EMT, K)
     print('start clustering')
